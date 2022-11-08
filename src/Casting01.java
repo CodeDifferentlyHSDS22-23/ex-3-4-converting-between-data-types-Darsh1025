c6500b9e61797ab9f1f9231f0d55339d0e5dc4f0
Darsh1025 <pateldarsh613.com>
2022-11-08 17:31:02 -0500

    updates

--- src/Casting01.java
+++ src/Casting01.java
@@ -2,11 +2,12 @@ public class Casting01 {
     public static void main(String[] args) {
         //Declare and initialize a byte with a value of 128
         //Observe NetBeans' complaint
+        byte value1 = 128;
+        //The byte value is being read in as an int
 
-        
         //Declare and initialize a short with a value of 128
         //Create a print statement that casts this short to a byte
-
+        short value2 = 128;
         
         
         //Declare and initialize a byte with a value of 127
