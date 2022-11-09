40063a69925cb0824735887828d6fa9bd8e21cd3
Darsh1025 <pateldarsh613.com>
2022-11-09 18:56:56 -0500

    Completed assignment

--- src/Casting01.java
+++ src/Casting01.java
@@ -2,20 +2,19 @@ public class Casting01 {
     public static void main(String[] args) {
         //Declare and initialize a byte with a value of 128
         //Observe NetBeans' complaint
-        byte value1 = 128;
-        //The byte value is being read in as an int
+        int value1 = 128;
+        //The byte value is being read in as an int when set at 128
 
         //Declare and initialize a short with a value of 128
         //Create a print statement that casts this short to a byte
         short value2 = 128;
-        
+        System.out.println((byte)(value2));
         
         //Declare and initialize a byte with a value of 127
         //Add 1 to this variable and print it
         //Add 1 to this variable again and print it again
-
-        
-        
-        
+        byte value3 = 127;
+        System.out.println(value3 + 1);
+        System.out.println(value3 + 1);
     }    
 }
--- src/Parsing01.java
+++ src/Parsing01.java
@@ -1,18 +1,19 @@
 public class Parsing01 {
     public static void main(String[] args) {
         //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
-        
-        
-        
-        
+        String shirtPrice = "15";
+        String taxRate = ".05";
+        String gibberish = "887ds7nds87dsfs";
+
         //Parse shirtPrice and taxRate, and print the total tax
-        
-        
+        int intshirtPrice = Integer.parseInt("15");
+        double doubletaxRate = Double.parseDouble(".05");
+        System.out.println(intshirtPrice * doubletaxRate);
         
         //Try to parse taxRate as an int
-        
+        int inttaxRate = Integer.parseInt(".05");
         //Try to parse gibberish as an int
-        
+        int intgibberish1 = Integer.parseInt("887ds7nds87dsfs");
     }
     
 }
